998a183f8083e1cd7adf89c3b855044e5ce08cd1
OAnisimov <OAnisimov@it-one.ru>
2022-01-04 21:54:18 +0300

    неявный параметр функции - arguments. Массивоподобная структура (объект), имеющая свойство length. Позволяет получить доступ ко всем аргументам функции

--- JS/reading NInja/function ch 3/Arguments.js
+++ JS/reading NInja/function ch 3/Arguments.js
@@ -1,9 +1,19 @@
-function sum() {
+function sumArguments() {
     let sum = 0;
-    for(let i = 0; i < arguments.length; i++){
+    for(let i = 0; i < arguments.length; i++){ // arguments - объект
         sum += arguments[i];
     }
     return sum;
 }
 
-console.log(sum(1, 2, 3));
+console.log(sumArguments(1, 2, 3));
+
+function sumRest(...rest) { // rest - массив
+    let sum = 0;
+    for (let i = 0; i < rest.length; i++) {
+        sum += rest[i]
+    }
+    return sum;
+}
+
+console.log(sumRest(1, 2, 3));

6fd4337bedf16ff770f14ae457102a04286e65e5
OAnisimov <OAnisimov@it-one.ru>
2022-01-05 20:36:10 +0300

    this

--- JS/reading NInja/function ch 3/this.js
+++ JS/reading NInja/function ch 3/this.js
@@ -32,6 +32,26 @@ let obj3 = {
     whatThis: varFunction
 }
 
-console.log(obj1.whatThis());
-console.log(obj2.whatThis());
-console.log(obj3.whatThis());
+// console.log(obj1.whatThis());
+// console.log(obj2.whatThis());
+// console.log(obj3.whatThis());
+
+
+/*
+* If the constructor returns an object, that object is returned as the value of the
+whole new expression, and the newly constructed object passed as this to the
+constructor is discarded.
+■ If, however, a nonobject is returned from the constructor, the returned value is
+ignored, and the newly created object is returned.*/
+var puppet = {
+    rules: false
+};
+function Emperor() { // если конструктор возвращает объект, то при исп-ие слова new возвращатеся этот же объект
+    this.rules = true;
+    return puppet;
+}
+var emperor = new Emperor();
+
+console.log(emperor.rules);
+console.log(puppet.rules);
+console.log((puppet === emperor));
